c723214b8664e828cfa5f85ab921fb4a701e98d9
shilko2013 <shilko2013@mail.ru>
2019-08-02 19:09:41 +0300

    Producer send added.

--- src/main/java/com/hireright/rabbitmq/producer/RabbitMQProducer.java
+++ src/main/java/com/hireright/rabbitmq/producer/RabbitMQProducer.java
@@ -1,9 +1,11 @@
 package com.hireright.rabbitmq.producer;
 
 import com.hireright.util.PropertiesReader;
+import com.rabbitmq.client.AMQP.Queue;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,17 +21,38 @@ public class RabbitMQProducer implements AutoCloseable { //TODO: Logger
             + File.pathSeparator + "rabbitmq.properties";
     private static int DEFAULT_NUMBER_TASK_THREADS = 10;
     private static int DEFAULT_CLOSE_TIME_SECONDS = 1;
+    private static String DEFAULT_EXCHANGE_NAME = "default_exchange_name";
+    private static String DEFAULT_QUEUE_NAME = "default_queue_name";
 
     private final ConnectionFactory connectionFactory;
-    private Connection connection;
     private Channel channel;
     private final ExecutorService executor;
     private int closeTimeSeconds;
 
     static {
         PropertiesReader propertiesReader = new PropertiesReader(defaultPathToResources);
+        setDefaultFields(propertiesReader);
+    }
+
+    private static void setDefaultFields(PropertiesReader propertiesReader) {
         setDefaultNumberTaskThreads(propertiesReader);
         setDefaultCloseTimeSeconds(propertiesReader);
+        setDefaultExchangeName(propertiesReader);
+        setDefaultQueueName(propertiesReader);
+    }
+
+    private static void setDefaultQueueName(PropertiesReader propertiesReader) {
+        String queueName = propertiesReader.getProperty("producer.queue.name");
+        if (queueName == null) //default init value
+            return;
+        DEFAULT_QUEUE_NAME = queueName;
+    }
+
+    private static void setDefaultExchangeName(PropertiesReader propertiesReader) {
+        String exchangeName = propertiesReader.getProperty("producer.exchange.name");
+        if (exchangeName == null) //default init value
+            return;
+        DEFAULT_EXCHANGE_NAME = exchangeName;
     }
 
     private static void setDefaultNumberTaskThreads(PropertiesReader propertiesReader) {
@@ -77,9 +100,38 @@ public class RabbitMQProducer implements AutoCloseable { //TODO: Logger
         return connectionFactory;
     }
 
-    public void openChannel() throws IOException, TimeoutException {
-        connection = connectionFactory.newConnection();
-        channel = connection.createChannel();
+    private Channel getChannel() throws IOException, TimeoutException {
+        if (channel != null)
+            return channel;
+        return initChannel();
+    }
+
+    private Channel initChannel() throws IOException, TimeoutException {
+        channel = connectionFactory.newConnection().createChannel();
+        channel.exchangeDeclare(DEFAULT_EXCHANGE_NAME, "direct", true);
+        return channel;
+    }
+
+    public Queue.DeclareOk createQueue() throws IOException {
+        return createQueue(DEFAULT_QUEUE_NAME);
+    }
+
+    public Queue.DeclareOk createQueue(String queueName) throws IOException {
+        return createQueue(queueName, queueName);
+    }
+
+    public Queue.DeclareOk createQueue(String queueName, String routingKey) throws IOException {
+        Queue.DeclareOk declareOk = channel.queueDeclare(queueName, true, false, false, null);
+        channel.queueBind(queueName, DEFAULT_EXCHANGE_NAME, routingKey);
+        return declareOk;
+    }
+
+    public void sendMessage(String message) throws IOException {
+        sendMessage(DEFAULT_QUEUE_NAME, message);
+    }
+
+    public void sendMessage(String routingKey, String message) throws IOException {
+        channel.basicPublish(DEFAULT_EXCHANGE_NAME, routingKey, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
     }
 
     @Override
@@ -87,6 +139,7 @@ public class RabbitMQProducer implements AutoCloseable { //TODO: Logger
         executor.shutdown();
         if (!executor.awaitTermination(closeTimeSeconds, TimeUnit.SECONDS))
             throw new TimeoutException();
+        Connection connection = channel.getConnection();
         channel.close();
         connection.close();
     }
