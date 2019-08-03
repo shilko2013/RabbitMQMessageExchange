cf1368e2a85aa29545ab1dc128e87d945d83dc31
shilko2013 <shilko2013@mail.ru>
2019-08-03 15:13:43 +0300

    Logger in RabbitMQProducer added.

--- src/main/java/com/hireright/rabbitmq/producer/RabbitMQProducer.java
+++ src/main/java/com/hireright/rabbitmq/producer/RabbitMQProducer.java
@@ -3,11 +3,15 @@ package com.hireright.rabbitmq.producer;
 import com.hireright.rabbitmq.RabbitMQClient;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.MessageProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 public class RabbitMQProducer extends RabbitMQClient implements MessageProducer {
+
+    private Logger log = LoggerFactory.getLogger(RabbitMQProducer.class);
 
     public RabbitMQProducer() {
         super();
@@ -24,6 +28,8 @@ public class RabbitMQProducer extends RabbitMQClient implements MessageProducer 
 
     @Override
     public void sendMessage(String routingKey, String message) throws IOException, TimeoutException {
+        String shortMessage = message.length() > 50 ? message.substring(50) + "..." : message;
+        log.debug("Sending message with routing key = " + routingKey + " with body: " + shortMessage);
         getChannel().basicPublish(getDefaultExchangeName(), routingKey, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
     }
 
