a85e421b8734ae01799704d36139a585a05a701d
shilko2013 <shilko2013@mail.ru>
2019-08-03 15:20:21 +0300

    Logger added in RabbitMQConsumer and SimpleConsumer.

--- src/main/java/com/hireright/rabbitmq/consumer/RabbitMQConsumer.java
+++ src/main/java/com/hireright/rabbitmq/consumer/RabbitMQConsumer.java
@@ -3,11 +3,15 @@ package com.hireright.rabbitmq.consumer;
 import com.hireright.rabbitmq.RabbitMQClient;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Consumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 public class RabbitMQConsumer extends RabbitMQClient implements MessageConsumer {
+
+    private Logger log = LoggerFactory.getLogger(RabbitMQConsumer.class);
 
     private boolean autoAck = false;
 
@@ -41,6 +45,7 @@ public class RabbitMQConsumer extends RabbitMQClient implements MessageConsumer 
 
     @Override
     public void addConsumer(String queueName, boolean autoAck, String consumerTag, Consumer consumer) throws IOException, TimeoutException {
+        log.debug("Consumer added in queue " + queueName + ", consumer tag = " + consumerTag);
         getChannel().basicConsume(queueName, autoAck, consumerTag, consumer);
     }
 }
--- src/main/java/com/hireright/util/SimpleConsumer.java
+++ src/main/java/com/hireright/util/SimpleConsumer.java
@@ -4,23 +4,31 @@ import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.function.Consumer;
 
 public class SimpleConsumer extends DefaultConsumer {
 
+    private static Logger log = LoggerFactory.getLogger(SimpleConsumer.class);
+
     private final Consumer<String> consumer;
 
     public SimpleConsumer(Channel channel, Consumer<String> consumer) {
         super(channel);
         this.consumer = consumer;
+        log.debug("Consumer created.");
     }
 
     @Override
     public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+        log.debug("Message has been received");
         long deliveryTag = envelope.getDeliveryTag();
+        log.debug("Consumer running...");
         consumer.accept(new String(body));
+        log.debug("Acknowledging queue...");
         getChannel().basicAck(deliveryTag, false);
     }
 }
