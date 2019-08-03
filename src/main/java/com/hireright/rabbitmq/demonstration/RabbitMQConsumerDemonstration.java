950a22b39af46734412234e73e3d5fa099fa9eae
shilko2013 <shilko2013@mail.ru>
2019-08-03 13:08:29 +0300

    ConsumerDemo ready.

--- src/main/java/com/hireright/rabbitmq/demonstration/RabbitMQConsumerDemonstration.java
+++ src/main/java/com/hireright/rabbitmq/demonstration/RabbitMQConsumerDemonstration.java
@@ -3,12 +3,15 @@ package com.hireright.rabbitmq.demonstration;
 import com.hireright.rabbitmq.consumer.RabbitMQConsumer;
 import com.hireright.util.SimpleConsumer;
 
+import java.util.Scanner;
+
 public class RabbitMQConsumerDemonstration {
 
     public static void main(String[] args) {
-        System.out.println("See the messages below: ");
+        System.out.println("Type ENTER to exit. See the messages below: ");
         try (RabbitMQConsumer consumer = new RabbitMQConsumer()) {
             consumer.addConsumer("consumerTag", new SimpleConsumer(consumer.getChannel(), System.out::println));
+            while (System.in.read() == -1);
         } catch (Exception e) {
             e.printStackTrace();
         }
