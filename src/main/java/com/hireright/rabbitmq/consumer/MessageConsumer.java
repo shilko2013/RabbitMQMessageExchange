f30d9fe554e1a44c0792b42b36fc4fa30388c3c5
shilko2013 <shilko2013@mail.ru>
2019-08-03 10:57:47 +0300

    MessageConsumer's methods added.

--- src/main/java/com/hireright/rabbitmq/consumer/MessageConsumer.java
+++ src/main/java/com/hireright/rabbitmq/consumer/MessageConsumer.java
@@ -1,7 +1,15 @@
 package com.hireright.rabbitmq.consumer;
 
+import com.rabbitmq.client.Consumer;
+
 public interface MessageConsumer {
 
+    void addConsumer(String consumerTag, Consumer consumer);
 
+    void addConsumer(boolean autoAck, String consumerTag, Consumer consumer);
+
+    void addConsumer(String queueName, String consumerTag, Consumer consumer);
+
+    void addConsumer(String queueName, boolean autoAck, String consumerTag, Consumer consumer);
 
 }
--- src/main/java/com/hireright/rabbitmq/consumer/RabbitMQConsumer.java
+++ src/main/java/com/hireright/rabbitmq/consumer/RabbitMQConsumer.java
@@ -2,8 +2,11 @@ package com.hireright.rabbitmq.consumer;
 
 import com.hireright.rabbitmq.RabbitMQClient;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
 
 public class RabbitMQConsumer extends RabbitMQClient implements MessageConsumer {
+
+    private boolean autoAck = false;
 
     public RabbitMQConsumer() {
         super();
@@ -13,6 +16,29 @@ public class RabbitMQConsumer extends RabbitMQClient implements MessageConsumer 
         super(connectionFactory);
     }
 
+    public RabbitMQConsumer(ConnectionFactory connectionFactory, boolean autoAck) {
+        super(connectionFactory);
+        this.autoAck = autoAck;
+    }
 
-    
+
+    @Override
+    public void addConsumer(String consumerTag, Consumer consumer) {
+
+    }
+
+    @Override
+    public void addConsumer(boolean autoAck, String consumerTag, Consumer consumer) {
+
+    }
+
+    @Override
+    public void addConsumer(String queueName, String consumerTag, Consumer consumer) {
+
+    }
+
+    @Override
+    public void addConsumer(String queueName, boolean autoAck, String consumerTag, Consumer consumer) {
+
+    }
 }
