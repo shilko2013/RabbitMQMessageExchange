97bd6e3c29f3cefc011a6d967dbb47d3db244909
shilko2013 <shilko2013@mail.ru>
2019-08-03 15:09:48 +0300

    Logger in RabbitMQClient added.

--- src/main/java/com/hireright/rabbitmq/RabbitMQClient.java
+++ src/main/java/com/hireright/rabbitmq/RabbitMQClient.java
@@ -1,13 +1,16 @@
 package com.hireright.rabbitmq;
 
-import com.hireright.util.PropertiesReader;
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeoutException;
 
 public abstract class RabbitMQClient implements AutoCloseable {
@@ -21,28 +24,32 @@ public abstract class RabbitMQClient implements AutoCloseable {
     private final ConnectionFactory connectionFactory;
     private Channel channel;
 
+    private final static Logger log = LoggerFactory.getLogger(RabbitMQClient.class);
+
     static {
-        PropertiesReader propertiesReader = new PropertiesReader(defaultPathToResources);
-        setDefaultFields(propertiesReader);
+        try (FileReader reader = new FileReader(defaultPathToResources)) {
+            Properties properties = new Properties();
+            properties.load(reader);
+            log.debug("Reading config from " + defaultPathToResources);
+            setDefaultFields(properties);
+        } catch (Exception e) {
+            log.debug("Config file " + defaultPathToResources + " doesn't exist. Using default values...");
+        }
     }
 
-    private static void setDefaultFields(PropertiesReader propertiesReader) {
-        setDefaultExchangeName(propertiesReader);
-        setDefaultQueueName(propertiesReader);
+    private static void setDefaultFields(Properties properties) {
+        setDefaultExchangeName(properties);
+        setDefaultQueueName(properties);
     }
 
-    private static void setDefaultQueueName(PropertiesReader propertiesReader) {
-        String queueName = propertiesReader.getProperty("rabbitmq.queue.name");
-        if (queueName == null) //default init value
-            return;
-        DEFAULT_QUEUE_NAME = queueName;
+    private static void setDefaultQueueName(Properties properties) {
+        DEFAULT_QUEUE_NAME = properties.getProperty("rabbitmq.queue.name");
+        log.debug("Default queue name from file: " + DEFAULT_QUEUE_NAME);
     }
 
-    private static void setDefaultExchangeName(PropertiesReader propertiesReader) {
-        String exchangeName = propertiesReader.getProperty("rabbitmq.exchange.name");
-        if (exchangeName == null) //default init value
-            return;
-        DEFAULT_EXCHANGE_NAME = exchangeName;
+    private static void setDefaultExchangeName(Properties properties) {
+        DEFAULT_EXCHANGE_NAME = properties.getProperty("rabbitmq.exchange.name");
+        log.debug("Default exchange name from file: " + DEFAULT_EXCHANGE_NAME);
     }
 
     public RabbitMQClient() {
@@ -51,6 +58,7 @@ public abstract class RabbitMQClient implements AutoCloseable {
 
     public RabbitMQClient(ConnectionFactory connectionFactory) {
         this.connectionFactory = connectionFactory;
+        log.debug("Init client with connection factory");
     }
 
     public Channel getChannel() throws IOException, TimeoutException {
@@ -60,7 +68,9 @@ public abstract class RabbitMQClient implements AutoCloseable {
     }
 
     private Channel initChannel() throws IOException, TimeoutException {
+        log.debug("Opening channel...");
         channel = connectionFactory.newConnection().createChannel();
+        log.debug("Creating exchange...");
         channel.exchangeDeclare(DEFAULT_EXCHANGE_NAME, "direct", true);
         createQueue();
         return channel;
@@ -75,7 +85,9 @@ public abstract class RabbitMQClient implements AutoCloseable {
     }
 
     public AMQP.Queue.DeclareOk createQueue(String queueName, String routingKey) throws IOException {
+        log.debug("Creating queue with queue name " + queueName + "...");
         AMQP.Queue.DeclareOk declareOk = channel.queueDeclare(queueName, true, false, false, null);
+        log.debug("Binding exchange and queue...");
         channel.queueBind(queueName, DEFAULT_EXCHANGE_NAME, routingKey);
         return declareOk;
     }
@@ -99,7 +111,9 @@ public abstract class RabbitMQClient implements AutoCloseable {
     @Override
     public void close() throws Exception {
         Connection connection = channel.getConnection();
+        log.debug("Closing channel...");
         channel.close();
+        log.debug("Closing connection...");
         connection.close();
     }
 }
