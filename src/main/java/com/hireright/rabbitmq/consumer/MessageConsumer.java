38e07fc4ed0e5e6feb587807a62bf4a5b9e6b864
shilko2013 <shilko2013@mail.ru>
2019-08-03 11:04:33 +0300

    MessageConsumer's methods override.

--- src/main/java/com/hireright/rabbitmq/consumer/MessageConsumer.java
+++ src/main/java/com/hireright/rabbitmq/consumer/MessageConsumer.java
@@ -4,12 +4,12 @@ import com.rabbitmq.client.Consumer;
 
 public interface MessageConsumer {
 
-    void addConsumer(String consumerTag, Consumer consumer);
+    void addConsumer(String consumerTag, Consumer consumer) throws Exception;
 
-    void addConsumer(boolean autoAck, String consumerTag, Consumer consumer);
+    void addConsumer(boolean autoAck, String consumerTag, Consumer consumer) throws Exception;
 
-    void addConsumer(String queueName, String consumerTag, Consumer consumer);
+    void addConsumer(String queueName, String consumerTag, Consumer consumer) throws Exception;
 
-    void addConsumer(String queueName, boolean autoAck, String consumerTag, Consumer consumer);
+    void addConsumer(String queueName, boolean autoAck, String consumerTag, Consumer consumer) throws Exception;
 
 }
--- src/main/java/com/hireright/rabbitmq/consumer/RabbitMQConsumer.java
+++ src/main/java/com/hireright/rabbitmq/consumer/RabbitMQConsumer.java
@@ -3,6 +3,10 @@ package com.hireright.rabbitmq.consumer;
 import com.hireright.rabbitmq.RabbitMQClient;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 public class RabbitMQConsumer extends RabbitMQClient implements MessageConsumer {
 
@@ -21,24 +25,23 @@ public class RabbitMQConsumer extends RabbitMQClient implements MessageConsumer 
         this.autoAck = autoAck;
     }
 
-
     @Override
-    public void addConsumer(String consumerTag, Consumer consumer) {
-
+    public void addConsumer(String consumerTag, Consumer consumer) throws IOException, TimeoutException {
+        addConsumer(getDefaultExchangeName(), autoAck, consumerTag, consumer);
     }
 
     @Override
-    public void addConsumer(boolean autoAck, String consumerTag, Consumer consumer) {
-
+    public void addConsumer(boolean autoAck, String consumerTag, Consumer consumer) throws IOException, TimeoutException {
+        addConsumer(getDefaultQueueName(), autoAck, consumerTag, consumer);
     }
 
     @Override
-    public void addConsumer(String queueName, String consumerTag, Consumer consumer) {
-
+    public void addConsumer(String queueName, String consumerTag, Consumer consumer) throws IOException, TimeoutException {
+        addConsumer(queueName, autoAck, consumerTag, consumer);
     }
 
     @Override
-    public void addConsumer(String queueName, boolean autoAck, String consumerTag, Consumer consumer) {
-
+    public void addConsumer(String queueName, boolean autoAck, String consumerTag, Consumer consumer) throws IOException, TimeoutException {
+        getChannel().basicConsume(queueName, autoAck, consumerTag, consumer);
     }
 }
